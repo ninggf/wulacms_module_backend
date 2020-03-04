360059ecbf1a29030940c7f86cce0aec76664752
windywany <windywany@163.com>
2020-03-04 11:43:59 +0800

    new start for v3.0

--- bootstrap.php
+++ bootstrap.php
@@ -2,14 +2,8 @@
 
 namespace backend;
 
-use backend\classes\AdminPassport;
-use backend\classes\CommonSetting;
 use wula\cms\CmfModule;
 use wulaphp\app\App;
-use wulaphp\auth\Passport;
-use wulaphp\io\Ajax;
-use wulaphp\io\Request;
-use wulaphp\io\Response;
 
 /**
  * 管理后台模块
@@ -21,7 +15,7 @@ class BackendModule extends CmfModule {
     }
 
     public function getDescription() {
-        return '基于bootstrap&layui的管理后台。';
+        return '基于layui与vuejs的易扩展管理后台界面';
     }
 
     public function getHomePageURL() {
@@ -32,85 +26,11 @@ class BackendModule extends CmfModule {
         return 'Leo Ning';
     }
 
-    /**
-     * @param Passport $passport
-     *
-     * @filter passport\newAdminPassport
-     *
-     * @return Passport
-     */
-    public static function createAdminPassport($passport) {
-        if ($passport instanceof Passport) {
-            $passport = new AdminPassport();
-        }
-
-        return $passport;
-    }
-
-    /**
-     * 需要登录才能操作.
-     *
-     * @param $view
-     *
-     * @filter mvc\admin\needLogin
-     * @return \wulaphp\mvc\view\View
-     */
-    public static function onNeedLogin($view) {
-        if (Request::isAjaxRequest()) {
-            Response::respond(401, __('Please login'));
-        } else {
-            App::redirect('backend/auth');
-        }
-
-        return $view;
-    }
-
-    /**
-     * @param mixed  $view
-     * @param string $message
-     *
-     * @filter mvc\admin\onDenied $message
-     * @return \wulaphp\mvc\view\View
-     */
-    public static function onDenied($view, $message) {
-        if (Request::isAjaxRequest()) {
-            $view = Ajax::fatal($message ? $message : __('permission denied'), 403);
-        } else {
-            Response::respond(403, $message ? $message : __('permission denied'));
-        }
-
-        return $view;
-    }
-
-    /**
-     * 用户被禁用了。
-     *
-     * @param \wulaphp\mvc\view\View $view
-     *
-     * @filter mvc\admin\onLocked
-     * @return \wulaphp\mvc\view\View
-     */
-    public static function onLocked($view) {
-        if (Request::isAjaxRequest()) {
-            $view = Ajax::fatal('You are blocked', 403);
-        }
-
-        return $view;
-    }
-
     public function getVersionList() {
         $v['1.0.0'] = '管理后台的第一个版本';
-        $v['2.0.0'] = '不再支持 php 5.6.x版本';
-        $v['2.1.0'] = '优化界面';
 
         return $v;
     }
-}
-
-function get_system_settings() {
-    return apply_filter('backend/settings', [
-        'default' => new CommonSetting(),
-    ]);
 }
 
 App::register(new BackendModule());
--- views/index/widgetCfg.tpl
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid wulaui m-t-sm">
-    <form id="widget-cfg-form" name="WidgetCfgForm" data-validate="{$rules|escape}" action="{'backend/save-cfg'|app}"
-          data-ajax method="post" data-loading>
-        <input type="hidden" name="id" id="id" value="{$id}"/>
-        {$form|render}
-    </form>
-</div>
