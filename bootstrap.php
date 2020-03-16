b16de3cef444f6b392e5b73de2be0afeb1cac3fe
windywany <windywany@163.com>
2020-03-16 17:39:15 +0800

    login and backend page

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,9 @@ namespace backend;
 
 use wula\cms\CmfModule;
 use wulaphp\app\App;
+use wulaphp\io\Ajax;
+use wulaphp\io\Request;
+use wulaphp\io\Response;
 
 /**
  * 管理后台模块
@@ -30,6 +33,57 @@ class BackendModule extends CmfModule {
         $v['1.0.0'] = '管理后台的第一个版本';
 
         return $v;
+    }
+
+    /**
+     * 需要登录才能操作.
+     *
+     * @param $view
+     *
+     * @filter mvc\admin\needLogin
+     * @return \wulaphp\mvc\view\View
+     */
+    public static function onNeedLogin($view) {
+        if (Request::isAjaxRequest()) {
+            Response::respond(401, __('Please login'));
+        } else {
+            App::redirect('backend/login');
+        }
+
+        return $view;
+    }
+
+    /**
+     * @param mixed  $view
+     * @param string $message
+     *
+     * @filter mvc\admin\onDenied $message
+     * @return \wulaphp\mvc\view\View
+     */
+    public static function onDenied($view, $message) {
+        if (Request::isAjaxRequest()) {
+            $view = Ajax::fatal($message ? $message : __('permission denied'), 403);
+        } else {
+            Response::respond(403, $message ? $message : __('permission denied'));
+        }
+
+        return $view;
+    }
+
+    /**
+     * 用户被禁用了。
+     *
+     * @param \wulaphp\mvc\view\View $view
+     *
+     * @filter mvc\admin\onLocked
+     * @return \wulaphp\mvc\view\View
+     */
+    public static function onLocked($view) {
+        if (Request::isAjaxRequest()) {
+            $view = Ajax::fatal('You are blocked', 403);
+        }
+
+        return $view;
     }
 }
 
