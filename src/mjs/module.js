076b87697baef99e720d907684c39e1294b62ad7
fym <373788426@qq.com>
2020-07-30 11:30:20 +0800

    '优化'

--- src/mjs/module.js
+++ src/mjs/module.js
@@ -7,30 +7,6 @@ layui.define(['layer','jquery'], (exports) => {
                 el     : '#module',
                 data   : {
                     list:[
-                        // {
-                        //     id:1,
-                        //     name:"模块一",
-                        //     width:"25",
-                        //     isadd:0,
-                        // },
-                        // {
-                        //     id:2,
-                        //     name:"模块二",
-                        //     width:"50",
-                        //     isadd:0,
-                        // },
-                        // {
-                        //     id:3,
-                        //     name:"模块三",
-                        //     width:"75",
-                        //     isadd:0,
-                        // },
-                        // {
-                        //     id:4,
-                        //     name:"模块四",
-                        //     width:"100",
-                        //     isadd:0,
-                        // },
                     ],
                     module_list:[],
                     //控制侧边栏开启关闭
@@ -40,9 +16,8 @@ layui.define(['layer','jquery'], (exports) => {
                     mod_show:0,
                 },
                 methods: {
-                    addModule(m){
-                        let el,item={},$vm=this;
-                            item=m;
+                    add(m){
+                        let el,$vm=this;
                             if(m.isadd){
                                 // 删除
                                 el=this.module_list.findIndex(function(c){
@@ -51,61 +26,30 @@ layui.define(['layer','jquery'], (exports) => {
                                 this.module_list.splice(el,1);
                             }else{
                                 //添加
-                                layui.use('@'+item.name, function(widget) {
-                                    $vm.module_list.push(item);
+                                layui.use('@'+m.name, function(widget) {
+                                    $vm.module_list.push(widget.cfg);
                                 }) 
                             }
                             m.isadd=!m.isadd;
-
-
-
                     },
                     saveModule(){
-                        //自定义模块保存暂时存入localstrog
-                        if(!window.localStorage){
-                            alert("浏览器不支持localstorage");
-                            return false;
-                        }else{
-                            window.localStorage.setItem('module_list',JSON.stringify(this.module_list));
-                            this.sid_show=1;
-                            layer.msg("保存成功")
-                        }
                     },
-                    initModule(){
-                        if(location.href.split('/')[location.href.split('/').length-1]!='backend'){
-                            this.mod_show=0;
-                        }else{
-                            this.mod_show=1;
-                        }
-                        //获取本地localstorge数据,显示已添加模块
-                        let stroge_module_list=JSON.parse(window.localStorage.getItem('module_list'));
-                        if(stroge_module_list && stroge_module_list.length){
-                            this.module_list=stroge_module_list;
-                            for (let item of this.module_list) {
-                                let index=this.list.findIndex((val)=>{
-                                    return val.id==item.id
-                                })
-                                this.list[index].isadd=1
-                                layui.use('@'+this.list[index].name, function(widget) {}) 
-                            }
-                        }
-                    },
-                    rander(){
+                    init(){
                         let $vm=this;
-                        layui.use(wlist, function(...widget) {
-                            console.log(widget)
-                            widget.forEach(item => {
-                                $vm.list.push(item.cfg)
-                            })
-                            $vm.initModule();
-                        }) 
-                    }
-                    
+                        this.list=wlist;
+                        this.mod_show=location.href.split('/')[location.href.split('/').length-1]!='backend'?0:1;
+                        this.list.forEach(item=>{
+                            if(item.isadd){
+                                layui.use('@'+item.name, function(widget) {
+                                    $vm.module_list.push(widget.cfg);
+                                }) 
+                            }
+                        })
+                    }   
                 },
                 mounted() {
                     console.log('module')
-                    this.list=wlist;
-                    this.initModule();
+                    this.init();
                 },
             }); 
         }
--- views/index.tpl
+++ views/index.tpl
@@ -1,17 +1,17 @@
 {literal}
 <div id="module" v-cloak v-show="mod_show">
     <span class="module-show" @click="sid_show=!sid_show;hide_sid=0 ">自定义</span>
-    <transition name="fade">
+
         <ul :class="{'hide':hide_sid}" v-show="sid_show">
             <i :class="[hide_sid?'layui-icon-right':'layui-icon-left','layui-icon']" @click="hide_sid=!hide_sid"></i>
             <li v-for="(item,index) in list">
                 <!--{{item.name}}-->
                 {{item.name}}
-                <i @click="addModule(item,index)" :class="[item.isadd?'layui-icon-ok':'layui-icon-addition','layui-icon']"></i>
+                <i @click="add(item,index)" :class="[item.isadd?'layui-icon-ok':'layui-icon-addition','layui-icon']"></i>
             </li>
             <li><span @click="hide_sid=1">取消</span><span @click="saveModule">保存</span></li>
         </ul>
-    </transition>
+    
     <transition name="fade">
         <div class="module-list" v-show="sid_show">
             <div v-for="(item,index) in module_list" :style="{'flex-basis':item.width+'%'}">{{item.name}}</div>
@@ -26,8 +26,8 @@
 <script>
     layui.use(['@backend.module'], function(mod) {
                mod.init([
-                   {id:1,name:'demo.widget',isadd:0,width:100},
-                   {id:2,name:'demo.widget2',isadd:0,width:50},
+                   {id:'demo.widget',name:'demo.widget',isadd:1},
+                   {id:'demo.widget2',name:'demo.widget2',isadd:0},
                ])
         
     }) 
